b268325248f62f2dc09641fbcc7239b02caf72bc
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2026-01-14 21:52:28 +0800

    enable see through password temp

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -45,7 +45,7 @@ const submit = async () => {
               id="password"
               v-model="credentials.password"
               label="Password"
-              type="password"
+              type="text"
             />
           </div>
         </div>
