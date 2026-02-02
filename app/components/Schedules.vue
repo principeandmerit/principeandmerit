a5b4633b357632e282a94a4ef8fbe87595d077b5
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2026-02-02 13:01:31 +0800

    Fix: pedia and ob schedules

--- app/components/Schedules.vue
+++ app/components/Schedules.vue
@@ -5,17 +5,17 @@
       style="background: white; border-radius: 8px; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),0 8px 24px rgba(0, 0, 0, 0.18);"
     >
       <ul>
-        <li class="mb-5">
+        <li>
           <h5 class="primary">
             OB
           </h5>
-          <span>Wednesday, Thursday, Friday 2PM - 4PM</span>
+          <span>Monday to Saturday 8AM - 1PM</span>
         </li>
-        <li>
+        <li class="mb-5">
           <h5 class="primary">
             Pedia
           </h5>
-          <span>Monday to Saturday 8AM - 1PM</span>
+          <span>Wednesday, Thursday, Friday 2PM - 4PM</span>
         </li>
       </ul>
     </div>
